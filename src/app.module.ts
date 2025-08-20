f36f771adeacbc7cbae05b7c0600c7f55803f52a
Affifa2003 <146504824+Affifa2003@users.noreply.github.com>
2025-08-20 12:05:56 +0530

    env and app.module update

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,17 +4,21 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { ContactModule } from './Contact/contact.module';
 import { QuickEnquiryModule } from './quick-enquiry/quick-enquiry.module';
 
-
-
 @Module({
   imports: [
     TypeOrmModule.forRoot({
       type: 'postgres',
-      host: 'localhost',
-      port: 5432,
-      username: 'postgres',
-      password: 'root',
-      database: 'contactdb',
+      host: process.env.DB_HOST || 'localhost',
+      port: parseInt(process.env.DB_PORT ?? '5432'),
+      username: process.env.DB_USER || 'postgres',
+      password: process.env.DB_PWD || 'root',
+      database: process.env.DB_NAME || 'contactdb',
+      // type: 'postgres',
+      // host: 'localhost',
+      // port: 5432,
+      // username: 'postgres',
+      // password: 'root',
+      // database: 'contactdb',
       autoLoadEntities: true,
       synchronize: true,
     }),
